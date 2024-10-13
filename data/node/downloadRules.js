e090d3066a1862fea7109392e5df26ea32b876bb
Linlin <1732509260@qq.com>
2024-10-13 15:54:28 +0800

    初始化

--- data/node/downloadRules.js
+++ data/node/downloadRules.js
@@ -1,37 +1,44 @@
-const fs = require('fs');
-const axios = require('axios');
-const iconv = require('iconv-lite');
-const path = require('path');
-const { promisify } = require('util');
+const fs = require("fs");
+const axios = require("axios");
+const iconv = require("iconv-lite");
+const path = require("path");
+const { promisify } = require("util");
 const writeFile = promisify(fs.writeFile);
 
 const downloadFile = async (url, directory) => {
-
-    try {
-        directory = path.join(__dirname, directory)
-        const response = await axios.get(url, { responseType: 'arraybuffer', timeout: 60000 });
-        const decoded = iconv.decode(response.data, 'utf8');
-        await writeFile(directory, decoded);
-        console.log(`下载规则文件成功: ${url}`);
-    } catch (error) {
-        throw `下载规则文件失败:${url}${error}`
-    }
-}
+  try {
+    directory = path.join(__dirname, directory);
+    const response = await axios.get(url, {
+      responseType: "arraybuffer",
+      timeout: 60000,
+    });
+    const decoded = iconv.decode(response.data, "utf8");
+    await writeFile(directory, decoded);
+    console.log(`下载规则文件成功: ${url}`);
+  } catch (error) {
+    console.log(`下载规则文件失败:${url}${error}`);
+    // throw `下载规则文件失败:${url}${error}`;
+  }
+};
 
 //规则下载
 const downloadRules = async (rules, allow, directory) => {
-
-    console.log('开始下载规则');
-    try {
-        const downloadPromises = rules.map((url, index) => downloadFile(url, `${directory}/rules${index}.txt`));
-        const allowPromises = allow.map((url, index) => downloadFile(url, `${directory}/allow${index}.txt`));
-        await Promise.all([...downloadPromises, ...allowPromises]);
-        console.log('规则下载完成');
-    } catch (error) {
-        throw `规则下载失败:${error}`
-    }
-}
+  console.log("开始下载规则");
+  try {
+    const downloadPromises = rules.map((url, index) =>
+      downloadFile(url, `${directory}/rules${index}.txt`)
+    );
+    const allowPromises = allow.map((url, index) =>
+      downloadFile(url, `${directory}/allow${index}.txt`)
+    );
+    await Promise.all([...downloadPromises, ...allowPromises]);
+    console.log("规则下载完成");
+  } catch (error) {
+    console.log(`规则下载失败:${error}`);
+    // throw `规则下载失败:${error}`;
+  }
+};
 
 module.exports = {
-    downloadRules
+  downloadRules,
 };
--- index.js
+++ index.js
@@ -1,113 +1,125 @@
 // 创建临时文件夹
-const { createDir } = require('./data/node/createDir'); // createDir.js 模块
+const { createDir } = require("./data/node/createDir"); // createDir.js 模块
 // 删除临时文件夹
-const { deleteDir } = require('./data/node/deleteDir'); // deleteDir.js 模块
+const { deleteDir } = require("./data/node/deleteDir"); // deleteDir.js 模块
 // 复制文件
-const { copyFiles } = require('./data/node/copyFiles'); // copyFiles.js 模块
+const { copyFiles } = require("./data/node/copyFiles"); // copyFiles.js 模块
 // 删除文件
-const { deleteFiles } = require('./data/node/deleteFiles'); // deleteFiles.js 模块
+const { deleteFiles } = require("./data/node/deleteFiles"); // deleteFiles.js 模块
 //规则下载
-const { downloadRules } = require('./data/node/downloadRules'); // downloadRules.js 模块
+const { downloadRules } = require("./data/node/downloadRules"); // downloadRules.js 模块
 // 添加空格
-const { addEmptyLinesToFiles } = require('./data/node/addEmptyLinesToFiles'); // addEmptyLinesToFiles.js 模块
+const { addEmptyLinesToFiles } = require("./data/node/addEmptyLinesToFiles"); // addEmptyLinesToFiles.js 模块
 // 规则转换
-const { transformations } = require('./data/node/transformations'); //transformations.js 模块
+const { transformations } = require("./data/node/transformations"); //transformations.js 模块
 // 合并规则
-const { processFiles } = require('./data/node/processFiles'); //processFiles.js 模块
+const { processFiles } = require("./data/node/processFiles"); //processFiles.js 模块
 // 过滤DNS
-const { filterDns } = require('./data/node/filterDns'); // 引用 filterDns.js 模块
+const { filterDns } = require("./data/node/filterDns"); // 引用 filterDns.js 模块
 // 规则处理
-const { rule } = require('./data/node/rule'); // rule.js 模块
+const { rule } = require("./data/node/rule"); // rule.js 模块
 // 处理title
-const { title } = require('./data/node/title'); // title.js 模块
+const { title } = require("./data/node/title"); // title.js 模块
 // 处理md文件
-const { cleanReadme } = require('./data/node/cleanReadme'); // cleanReadme.js 模块
+const { cleanReadme } = require("./data/node/cleanReadme"); // cleanReadme.js 模块
 
 //黑名单规则
 const rules = [
-    "https://raw.githubusercontent.com/qq5460168/dangchu/main/black.txt",//5460
-    "https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt",//大萌主
-    "https://raw.githubusercontent.com/afwfv/DD-AD/main/rule/DD-AD.txt",//DD
-    "https://raw.gitmirror.com/Cats-Team/dns-filter/main/abp.txt",//AdRules DNS Filter
-    "https://raw.hellogithub.com/hosts",//GitHub加速
-    "https://raw.githubusercontent.com/qq5460168/dangchu/main/adhosts.txt",//测试hosts
-    // "https://raw.githubusercontent.com/qq5460168/dangchu/main/white.txt",//白名单
-    "https://raw.githubusercontent.com/loveqqzj/AdGuard/master/Mobile.txt",//loveqqzj
-    "https://raw.githubusercontent.com/mphin/AdGuardHomeRules/main/Blacklist.txt",//mphin
-    "https://gitee.com/zjqz/ad-guard-home-dns/raw/master/black-list",//周木木
-    "https://raw.githubusercontent.com/liwenjie119/adg-rules/master/black.txt",//liwenjie119
-    "https://raw.githubusercontent.com/qq5460168/dangchu/main/T%E7%99%BD%E5%90%8D%E5%8D%95.txt",//T白
-    "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt",//秋风规则
-    "https://github.com/entr0pia/fcm-hosts/raw/fcm/fcm-hosts",//FCM Hosts
+  "https://raw.githubusercontent.com/qq5460168/dangchu/main/black.txt", //5460
+  "https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt", //大萌主
+  "https://raw.githubusercontent.com/afwfv/DD-AD/main/rule/DD-AD.txt", //DD
+  "https://raw.gitmirror.com/Cats-Team/dns-filter/main/abp.txt", //AdRules DNS Filter
+  "https://raw.hellogithub.com/hosts", //GitHub加速
+  "https://raw.githubusercontent.com/qq5460168/dangchu/main/adhosts.txt", //测试hosts
+  "https://raw.githubusercontent.com/qq5460168/dangchu/main/white.txt", //白名单
+  "https://raw.githubusercontent.com/loveqqzj/AdGuard/master/Mobile.txt", //loveqqzj
+  "https://raw.githubusercontent.com/mphin/AdGuardHomeRules/main/Blacklist.txt", //mphin
+  "https://gitee.com/zjqz/ad-guard-home-dns/raw/master/black-list", //周木木
+  "https://raw.githubusercontent.com/liwenjie119/adg-rules/master/black.txt", //liwenjie119
+  "https://raw.githubusercontent.com/qq5460168/dangchu/main/T%E7%99%BD%E5%90%8D%E5%8D%95.txt", //T白
+  "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt", //秋风规则
+  "https://github.com/entr0pia/fcm-hosts/raw/fcm/fcm-hosts", //FCM Hosts
 
-    // "https://www.coolapk.com/link?url=https%3A%2F%2Fraw.gitmirror.com%2FTTDNS%2FCat%2Fmain%2FTT%25E6%258B%2592%25E7%25BB%259D%25E5%2588%2597%25E8%25A1%25A8%25E6%25B8%2585%25E5%258D%2595%2520(%25E6%259B%25B4%25E6%2596%25B0%25E4%25B8%25AD).txt",//TTDNS
-    // "https://mirror.ghproxy.com/raw.githubusercontent.com/Lynricsy/HyperADRules/master/rules.txt",//HyperADRules
-    // "https://raw.githubusercontent.com/BlueSkyXN/AdGuardHomeRules/master/all.txt",//AdGuardHomeRules
-    // "https://raw.githubusercontent.com/BlueSkyXN/AdGuardHomeRules/master/manhua-max.txt",//AdGuardHomeRules
-    // "https://raw.githubusercontent.com/BlueSkyXN/AdGuardHomeRules/master/skyrules.txt",//AdGuardHomeRules
-    // "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/minority-mv.txt",//乘风视频
-    // "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt",//乘风视频
-    // "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt",//乘风视频
-    // "https://raw.githubusercontent.com/Zisbusy/AdGuardHome-Rules/main/Rules/blacklist.txt",//Zisbusy
-    // "https://raw.githubusercontent.com/Zisbusy/AdGuardHome-Rules/main/Rules/easylist.txt",//Zisbusy
-    // "https://raw.githubusercontent.com/Zisbusy/AdGuardHome-Rules/main/Rules/filter.txt",//Zisbusy
-    // "https://raw.githubusercontent.com/sccheng460/adguard/main/blacklist.txt",//sccheng460
-]
+  "https://www.coolapk.com/link?url=https%3A%2F%2Fraw.gitmirror.com%2FTTDNS%2FCat%2Fmain%2FTT%25E6%258B%2592%25E7%25BB%259D%25E5%2588%2597%25E8%25A1%25A8%25E6%25B8%2585%25E5%258D%2595%2520(%25E6%259B%25B4%25E6%2596%25B0%25E4%25B8%25AD).txt", //TTDNS
+  "https://mirror.ghproxy.com/raw.githubusercontent.com/Lynricsy/HyperADRules/master/rules.txt", //HyperADRules
+  "https://raw.githubusercontent.com/BlueSkyXN/AdGuardHomeRules/master/all.txt", //AdGuardHomeRules
+  "https://raw.githubusercontent.com/BlueSkyXN/AdGuardHomeRules/master/manhua-max.txt", //AdGuardHomeRules
+  "https://raw.githubusercontent.com/BlueSkyXN/AdGuardHomeRules/master/skyrules.txt", //AdGuardHomeRules
+  "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/minority-mv.txt", //乘风视频
+  "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt", //乘风视频
+  "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt", //乘风视频
+  "https://raw.githubusercontent.com/Zisbusy/AdGuardHome-Rules/main/Rules/blacklist.txt", //Zisbusy
+  "https://raw.githubusercontent.com/Zisbusy/AdGuardHome-Rules/main/Rules/easylist.txt", //Zisbusy
+  "https://raw.githubusercontent.com/Zisbusy/AdGuardHome-Rules/main/Rules/filter.txt", //Zisbusy
+  "https://raw.githubusercontent.com/sccheng460/adguard/main/blacklist.txt", //sccheng460
+];
 //白名单规则
 const allow = [
-    "https://raw.githubusercontent.com/qq5460168/dangchu/main/white.txt",//白名单
-    "https://raw.githubusercontent.com/mphin/AdGuardHomeRules/main/Allowlist.txt",
-    "https://file-git.trli.club/file-hosts/allow/Domains",//冷漠
-    "https://hub.gitmirror.com/https://raw.githubusercontent.com/user001235/112/main/white.txt",//浅笑
-    "https://gitee.com/zjqz/ad-guard-home-dns/raw/master/white-list",//周木木
-    "https://raw.githubusercontent.com/liwenjie119/adg-rules/master/white.txt",//liwenjie119
-    "https://raw.githubusercontent.com/qq5460168/dangchu/main/T%E7%99%BD%E5%90%8D%E5%8D%95.txt",//T白名单
+  "https://raw.githubusercontent.com/qq5460168/dangchu/main/white.txt", //白名单
+  "https://raw.githubusercontent.com/mphin/AdGuardHomeRules/main/Allowlist.txt",
+  "https://file-git.trli.club/file-hosts/allow/Domains", //冷漠
+  "https://hub.gitmirror.com/https://raw.githubusercontent.com/user001235/112/main/white.txt", //浅笑
+  "https://gitee.com/zjqz/ad-guard-home-dns/raw/master/white-list", //周木木
+  "https://raw.githubusercontent.com/liwenjie119/adg-rules/master/white.txt", //liwenjie119
+  "https://raw.githubusercontent.com/qq5460168/dangchu/main/T%E7%99%BD%E5%90%8D%E5%8D%95.txt", //T白名单
 
-    "https://raw.githubusercontent.com/Zisbusy/AdGuardHome-Rules/main/Rules/whitelist.txt",//Zisbusy
-    "https://raw.githubusercontent.com/sccheng460/adguard/main/whitelist.txt",//sccheng460
-    // "https://mirror.ghproxy.com/raw.githubusercontent.com/8680/GOODBYEADS/master/allow.txt",//8680
-    "https://raw.githubusercontent.com/BlueSkyXN/AdGuardHomeRules/master/ok.txt",//AdGuardHomeRules
-]
+  "https://raw.githubusercontent.com/Zisbusy/AdGuardHome-Rules/main/Rules/whitelist.txt", //Zisbusy
+  "https://raw.githubusercontent.com/sccheng460/adguard/main/whitelist.txt", //sccheng460
+  "https://mirror.ghproxy.com/raw.githubusercontent.com/8680/GOODBYEADS/master/allow.txt", //8680
+  "https://raw.githubusercontent.com/BlueSkyXN/AdGuardHomeRules/master/ok.txt", //AdGuardHomeRules
+];
 // 旧地址
-const oldDirectory = './tmp'
+const oldDirectory = "./tmp";
 // 新地址
-const newDirectory = './'
+const newDirectory = "./";
 
 async function main() {
-    try {
-        // 创建临时文件夹
-        await createDir(oldDirectory)
-        //规则下载
-        await downloadRules(rules, allow, oldDirectory)
-        // 删除文件
-        await deleteFiles('./allow.txt', './dns.txt', './DnsConfiguration.txt', './rules.txt')
-        // 复制文件
-        await copyFiles('./data/rules/adblock.txt', `${oldDirectory}/rules01.txt`);
-        await copyFiles('./data/rules/whitelist.txt', `${oldDirectory}/allow01.txt`);
-        // 添加空格
-        await addEmptyLinesToFiles(oldDirectory);
-        // 规则转换
-        await transformations(oldDirectory);
-        // 合并规则
-        await processFiles(oldDirectory)
-        // 复制文件
-        await copyFiles(`${oldDirectory}/tmp-allow.txt`, `${newDirectory}/allow.txt`);
-        await copyFiles(`${oldDirectory}/tmp-rules.txt`, `${newDirectory}/rules.txt`);
-        // 过滤DNS
-        await filterDns(newDirectory)
-        // 规则处理
-        await rule(newDirectory)
-        // 处理title
-        await title()
-        // 处理md文件
-        await cleanReadme()
-        // 删除临时文件夹
-        await deleteDir(oldDirectory)
-        console.log('更新完成');
-
-    } catch (error) {
-        throw `更新失败:${error}`
-    }
-
+  try {
+    // 创建临时文件夹
+    await createDir(oldDirectory);
+    //规则下载
+    await downloadRules(rules, allow, oldDirectory);
+    // 删除文件
+    await deleteFiles(
+      "./allow.txt",
+      "./dns.txt",
+      "./DnsConfiguration.txt",
+      "./rules.txt"
+    );
+    // 复制文件
+    await copyFiles("./data/rules/adblock.txt", `${oldDirectory}/rules01.txt`);
+    await copyFiles(
+      "./data/rules/whitelist.txt",
+      `${oldDirectory}/allow01.txt`
+    );
+    // 添加空格
+    await addEmptyLinesToFiles(oldDirectory);
+    // 规则转换
+    await transformations(oldDirectory);
+    // 合并规则
+    await processFiles(oldDirectory);
+    // 复制文件
+    await copyFiles(
+      `${oldDirectory}/tmp-allow.txt`,
+      `${newDirectory}/allow.txt`
+    );
+    await copyFiles(
+      `${oldDirectory}/tmp-rules.txt`,
+      `${newDirectory}/rules.txt`
+    );
+    // 过滤DNS
+    await filterDns(newDirectory);
+    // 规则处理
+    await rule(newDirectory);
+    // 处理title
+    await title();
+    // 处理md文件
+    await cleanReadme();
+    // 删除临时文件夹
+    await deleteDir(oldDirectory);
+    console.log("更新完成");
+  } catch (error) {
+    throw `更新失败:${error}`;
+  }
 }
-main()
+main();
