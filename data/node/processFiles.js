6b133f9a8025fd396f857678129fb55e16809230
Linlin <1732509260@qq.com>
2024-10-31 09:50:48 +0800

    初始化

--- data/node/processFiles.js
+++ data/node/processFiles.js
@@ -1,51 +1,57 @@
-const fs = require('fs');
-const { promisify } = require('util');
-const path = require('path');
-const readDir = promisify(fs.readdir)
-const readFile = promisify(fs.readFile)
-const writeFile = promisify(fs.writeFile)
+const fs = require("fs");
+const { promisify } = require("util");
+const path = require("path");
+const readDir = promisify(fs.readdir);
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
 
 // 合并规则
 const processFiles = async (directory) => {
-    console.log('开始合并规则');
+  console.log("开始合并规则");
 
-    try {
-        directory = path.join(__dirname, directory)
-        // 读取列表文件名
-        const files = await readDir(directory)
-        // 过滤rules开头的txt文件
-        const ruleFiles = files.filter(file => file.startsWith('rules') && file.endsWith('.txt'));
-        // 读取所有文件内容
-        const allruleData = await Promise.all(ruleFiles.map(file => readFile(`${directory}/${file}`, 'utf8')));
-        // 处理文件规则
-        let allruleDatas = allruleData
-            .join('\n')
-            .split('\n')
-            .filter(line => !/^((\!)|(\[)).*/.test(line))
-            .join('\n')
-        // 写入文件
-        await writeFile(`${directory}/tmp-rules.txt`, allruleDatas, 'utf8')
+  try {
+    directory = path.join(__dirname, directory);
+    // 读取列表文件名
+    const files = await readDir(directory);
+    // 过滤rules开头的txt文件
+    const ruleFiles = files.filter(
+      (file) => file.startsWith("rules") && file.endsWith(".txt")
+    );
+    // 读取所有文件内容
+    const allruleData = await Promise.all(
+      ruleFiles.map((file) => readFile(`${directory}/${file}`, "utf8"))
+    );
+    // 处理文件规则
+    let allruleDatas = allruleData
+      .join("\n")
+      .split("\n")
+      .filter((line) => !/^((\!)|(\[)).*/.test(line))
+      .filter((line) => !/^!|^#[^#,^@,^%,^\$]|^\[.*\]$/.test(line))
+      .join("\n");
+    // 写入文件
+    await writeFile(`${directory}/tmp-rules.txt`, allruleDatas, "utf8");
 
-        // 读取列表文件名
-        const filess = await readDir(directory)
-        // 读取所有文件内容
-        const allallowData = await Promise.all(filess.map(file => readFile(`${directory}/${file}`, 'utf8')));
-        // 处理文件规则
-        let allallowDatas = allallowData
-            .join('\n')
-            .split('\n')
-            .filter(line => line.startsWith('@'))
-            .join('\n')
+    // 读取列表文件名
+    const filess = await readDir(directory);
+    // 读取所有文件内容
+    const allallowData = await Promise.all(
+      filess.map((file) => readFile(`${directory}/${file}`, "utf8"))
+    );
+    // 处理文件规则
+    let allallowDatas = allallowData
+      .join("\n")
+      .split("\n")
+      .filter((line) => line.startsWith("@"))
+      .join("\n");
 
-        // 写入文件
-        await writeFile(`${directory}/tmp-allow.txt`, allallowDatas, 'utf8')
+    // 写入文件
+    await writeFile(`${directory}/tmp-allow.txt`, allallowDatas, "utf8");
 
-        console.log('合并规则完成');
-
-    } catch (error) {
-        throw `合并规则失败:${error}`
-    }
-}
+    console.log("合并规则完成");
+  } catch (error) {
+    throw `合并规则失败:${error}`;
+  }
+};
 module.exports = {
-    processFiles
+  processFiles,
 };
--- data/node/rule.js
+++ data/node/rule.js
@@ -1,36 +1,38 @@
-const fs = require('fs');
-const { promisify } = require('util');
-const readDir = promisify(fs.readdir)
-const readFile = promisify(fs.readFile)
-const writeFile = promisify(fs.writeFile)
+const fs = require("fs");
+const { promisify } = require("util");
+const readDir = promisify(fs.readdir);
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
 
 // 规则去重
 const rule = async (directory) => {
-    console.log("开始规则去重");
-    try {
-        // 读取当前目录下的所有文件
-        const files = await readDir(directory).then(files => files.filter(file => file.endsWith('.txt')))
-        for (let index = 0; index < files.length; index++) {
-            const file = files[index]
-            // 读取文件内容
-            const data = await readFile(file, 'utf8')
-            // 将文件内容按行分割，‌去重，‌排序
-            const lines = [...new Set(data.split('\n'))]
+  console.log("开始规则去重");
+  try {
+    // 读取当前目录下的所有文件
+    const files = await readDir(directory).then((files) =>
+      files.filter((file) => file.endsWith(".txt"))
+    );
+    for (let index = 0; index < files.length; index++) {
+      const file = files[index];
+      // 读取文件内容
+      const data = await readFile(file, "utf8");
+      // 将文件内容按行分割，‌去重，‌排序
+      const lines = [...new Set(data.split("\n"))];
 
-            const linesdata = lines
-                .sort()
-                .filter(line => line.trim() !== "")
-                .map(line => line.trim())
-                .filter(line => !/(((^#)([^#]|$))|^#{4,}).*$/.test(line))
-                .join('\n')
-            await writeFile(file, linesdata, 'utf8')
-        }
-
-        console.log("规则去重成功");
-    } catch (error) {
-        throw `规则去重失败:${error}`
+      const linesdata = lines
+        .sort()
+        .filter((line) => line.trim() !== "")
+        .map((line) => line.trim())
+        .filter((line) => !/(((^#)([^#]|$))|^#{4,}).*$/.test(line))
+        .join("\n");
+      await writeFile(file, linesdata, "utf8");
     }
-}
+
+    console.log("规则去重成功");
+  } catch (error) {
+    throw `规则去重失败:${error}`;
+  }
+};
 module.exports = {
-    rule
+  rule,
 };
