1b9748650cc38ed53872fb258b31f489bddbf523
林＆林 <49576099+LINJIANPEI@users.noreply.github.com>
2024-12-20 07:12:34 +0800

    Update mergeBlacklists.js

--- data/node/mergeBlacklists.js
+++ data/node/mergeBlacklists.js
@@ -22,7 +22,7 @@ const mergeBlacklists = async (directory) => {
     const allFileDatas = allFileData
       .join("\n")
       .split("\n")
-      .filter((line) => !/^((\!)|(\[)).*/.test(line))
+      //.filter((line) => !/^((\!)|(\[)).*/.test(line))
       .join("\n");
     // 写入文件
     await writeFile(`${directory}/tmp-rules.txt`, allFileDatas, "utf8");
