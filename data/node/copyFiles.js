940383be2fd0ed2d2e898b431051c67db46bd037
Linlin <1732509260@qq.com>
2024-12-20 15:26:23 +0800

    更新

--- data/node/copyFiles.js
+++ data/node/copyFiles.js
@@ -1,29 +1,33 @@
 const fs = require("fs");
 const { promisify } = require("util");
 const copyFile = promisify(fs.copyFile);
+const access = promisify(fs.access);
 
-async function fileExistsAsync(filePath) {
+// 检查文件是否存在
+const fileExistsAsync = async (filePath) => {
   try {
-    await fs.access(filePath, fs.constants.F_OK);
+    await access(filePath, fs.constants.F_OK);
     return true;
   } catch (error) {
     return false;
   }
-}
+};
 
 // 复制文件
-const copyFiles = async (oldDirectory, newDirectory) => {
-  console.log("开始复制文件");
+const copyFiles = async (oldPath, newPath) => {
   try {
-    if (await fileExistsAsync(oldDirectory)) {
-      copyFile(oldDirectory, newDirectory);
-      console.log("复制文件成功");
+    // 检查源文件是否存在
+    if (await fileExistsAsync(oldPath)) {
+      // 复制文件并等待完成
+      await copyFile(oldPath, newPath);
+      console.log("文件复制成功");
+    } else {
+      console.error("源文件不存在");
     }
   } catch (error) {
-    throw `复制文件失败:${error}`;
+    console.error(`复制文件失败: ${error.message}`);
+    throw error; // 根据需要决定是否要重新抛出错误
   }
 };
 
-module.exports = {
-  copyFiles,
-};
+module.exports = { copyFiles };
