874d04aa9e0bf8f4578d57274e6a9f0519e388b2
Linlin <1732509260@qq.com>
2024-12-20 18:27:41 +0800

    优化

--- data/node/mergeBlacklists.js
+++ data/node/mergeBlacklists.js
@@ -10,25 +10,33 @@ const mergeBlacklists = async (directory) => {
   try {
     // 读取列表文件名
     const fileList = await readDir(directory);
-    // 过滤rules开头的txt文件
-    const fileLists = fileList.filter(
+    // 过滤出以"rules"开头且以".txt"结尾的文件
+    const rulesFiles = fileList.filter(
       (file) => file.startsWith("rules") && file.endsWith(".txt")
     );
+
+    // 如果没有找到符合条件的文件，打印消息并返回
+    if (rulesFiles.length === 0) {
+      console.log("没有找到符合条件的黑名单文件");
+      return;
+    }
+
     // 读取所有文件内容
     const allFileData = await Promise.all(
-      fileLists.map((file) => readFile(`${directory}/${file}`, "utf8"))
+      rulesFiles.map((file) => readFile(`${directory}/${file}`, "utf8"))
     );
     // 处理文件规则
     const allFileDatas = allFileData
       .join("\n")
       .split("\n")
-      //.filter((line) => !/^((\!)|(\[)).*/.test(line))
+      .filter((line) => !/^((\!)|(\[)).*/.test(line))
       .join("\n");
     // 写入文件
     await writeFile(`${directory}/tmp-rules.txt`, allFileDatas, "utf8");
-    console.log("合并黑名单规则完成");
+    console.log(`合并白名单规则完成，共处理了${rulesFiles.length}个文件`);
   } catch (error) {
-    throw `合并黑名单规则失败:${error}`;
+    console.error(`合并黑名单规则失败: ${error.message}`);
+    throw new Error(`合并黑名单规则失败: ${error.message}`);
   }
 };
 module.exports = {
--- data/node/mergeWhitelist.js
+++ data/node/mergeWhitelist.js
@@ -11,13 +11,18 @@ const mergeWhitelist = async (directory) => {
   try {
     // 读取列表文件名
     const fileList = await readDir(directory);
-    // 过滤rules开头的txt文件
-    const fileLists = fileList.filter(
+    // 过滤出以"allow"开头且以".txt"结尾的文件
+    const allowFiles = fileList.filter(
       (file) => file.startsWith("allow") && file.endsWith(".txt")
     );
-    // 读取所有文件内容
+    // 如果没有找到符合条件的文件，提前返回
+    if (allowFiles.length === 0) {
+      console.log("没有找到符合条件的白名单文件");
+      return;
+    }
+    // 读取所有符合条件的文件内容
     const allFileData = await Promise.all(
-      fileLists.map((file) => readFile(`${directory}/${file}`, "utf8"))
+      allowFiles.map((file) => readFile(`${directory}/${file}`, "utf8"))
     );
     // 处理文件规则
     const allFileDatas = allFileData
@@ -28,9 +33,10 @@ const mergeWhitelist = async (directory) => {
     // 写入文件
     await writeFile(`${directory}/tmp-allow.txt`, allFileDatas, "utf8");
 
-    console.log("合并白名单规则完成");
+    console.log(`合并白名单规则完成，共处理了${allowFiles.length}个文件`);
   } catch (error) {
-    throw `合并白名单规则失败:${error}`;
+    console.error(`合并白名单规则失败: ${error.message}`);
+    throw new Error(`合并白名单规则失败: ${error.message}`);
   }
 };
 module.exports = {
