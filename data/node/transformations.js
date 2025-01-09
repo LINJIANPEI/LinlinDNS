5f73b54289ec66c588eb99204361dcebaa54e757
Linlin <1732509260@qq.com>
2025-01-10 07:20:50 +0800

    更新

--- data/node/transformations.js
+++ data/node/transformations.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const { promisify } = require("util");
+const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 const compile = require("@adguard/hostlist-compiler");
 
