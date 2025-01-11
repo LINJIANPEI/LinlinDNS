995548f2c6db742e690726b7517bf416ee415597
Linlin <1732509260@qq.com>
2025-01-12 00:41:29 +0800

    更新

--- data/node/mergeBlacklists.js
+++ data/node/mergeBlacklists.js
@@ -41,9 +41,11 @@ const mergeBlacklists = async (directory) => {
           allFileDataFilter.push(line);
         }
       });
+    allFileDatas = filters(allFileDatas);
+    allFileDataFilter = filters(allFileDataFilter);
 
     console.log(`合并白名单规则完成，共处理了${rulesFiles.length}个文件`);
-    return [filters(allFileDatas), filters(allFileDataFilter)];
+    return [allFileDatas, allFileDataFilter];
   } catch (error) {
     throw new Error(`合并黑名单规则失败: ${error.message}`);
   }
