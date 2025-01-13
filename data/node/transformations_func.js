f5ba80b0e52391ca343f7d8fca612d42af7c7d78
Linlin <1732509260@qq.com>
2025-01-13 19:33:20 +0800

    更新

--- data/node/transformations_func.js
+++ data/node/transformations_func.js
@@ -79,7 +79,7 @@ const processRuleLines = async (lines, str = "") => {
       .map((line) => {
         // 判断是否为注释行或特定结构的行
         const isNotValidStructure = !/^##.*[\u4e00-\u9fa5].*$/.test(line);
-        const isNotValidStructures = !/^##.*\[.*\].*$/.test(line);
+        const isNotValidStructures = /^##.*\[.*\].*$/.test(line);
         // 如果符合条件（不是注释行或特定结构的行），为规则添加自定义前缀
         if (isNotValidStructure || isNotValidStructuress) {
           return line;
