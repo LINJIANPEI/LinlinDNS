eb0b2fa95407576880490112b8bd22ca584fa079
Linlin <1732509260@qq.com>
2025-02-07 21:29:29 +0800

    更新

--- data/node/modifiersFilter.js
+++ data/node/modifiersFilter.js
@@ -6,7 +6,7 @@ const modifiersFilter = async (con) => {
     const cons = con.map((line) => {
       return line
         .trim()
-        .replace(/^@@\|\||^\|\||^@@|\$important\$|\s#[^#]*$|\^$/, "");
+        .replace(/^@@\|\||^\|\||^\||^@@|\$important\$|\s#[^#]*$|\^$/, "");
     });
 
     console.log(`过滤修饰符成功`);
